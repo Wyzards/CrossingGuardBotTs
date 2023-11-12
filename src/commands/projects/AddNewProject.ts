c8d5682755ccc89e293b5ded5d4bf71f136664a6
Theeef <realstarkr@gmail.com>
2023-11-12 16:58:19 -0500

    cache

--- src/commands/projects/AddNewProject.ts
+++ src/commands/projects/AddNewProject.ts
@@ -1,9 +1,22 @@
 import { SlashCommandBuilder } from "discord.js";
 
-const data = new SlashCommandBuilder().setName("addproject").setDescription("Add an empty project with the given name");
+const data = new SlashCommandBuilder()
+    .setName("addproject")
+    .setDescription("Add an empty project with the given name")
+    .addStringOption(option =>
+        option.setName("project_name")
+            .setDescription("The internal name for this project")
+            .setRequired(true))
+    .addStringOption(option =>
+        option.setName("display_name")
+            .setDescription("The display name for this project")
+            .setRequired(true));
 
 async function execute(interaction) {
-    await interaction.reply("Project created!");
+    const projectName = interaction.options.getString("project_name");
+    const displayName = interaction.options.getString("display_name");
+
+    await interaction.reply("Project created with project_name: " + projectName + ", and display_name: " + displayName);
 }
 
 export { data, execute };
