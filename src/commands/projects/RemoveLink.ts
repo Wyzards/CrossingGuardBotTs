957d08a4cd4545c2871c840b0b43c8185042a512
Theeef <realstarkr@gmail.com>
2023-11-15 19:14:57 -0500

    remove link

---
+++ src/commands/projects/RemoveLink.ts
@@ -0,0 +1,29 @@
+import { SlashCommandBuilder, ForumChannel, CommandInteractionOptionResolver } from "discord.js";
+import CrossingGuardBot from "../../CrossingGuardBot";
+import ProjectLink from "../../ProjectLink";
+
+const data = new SlashCommandBuilder()
+    .setName("removelink")
+    .setDescription("Remove a link from a project")
+    .addStringOption(option =>
+        option.setName("project_name")
+            .setDescription("The name of the project")
+            .setRequired(true))
+    .addStringOption(option =>
+        option.setName("name")
+            .setDescription("The name to display for this link")
+            .setRequired(true));
+
+async function execute(interaction) {
+    const projectName = interaction.options.getString("project_name");
+    const linkName = interaction.options.getString("name");
+
+    CrossingGuardBot.getInstance().database.getProjectByName(projectName).then(project => {
+        project.links = project.links.filter(link => link.linkName !== linkName);
+        CrossingGuardBot.getInstance().database.saveProject(project);
+    });
+
+    interaction.reply("Removed the link `" + linkName + "` from " + projectName);
+}
+
+export { data, execute };
