fa93f551337e7bf6a0dacaebcb89d40f0b8cd1c3
Theeef <realstarkr@gmail.com>
2024-02-27 12:35:43 -0500

    any channel starting with hidden-announce is an announce channel

--- src/bot/events/messageCreate.ts
+++ src/bot/events/messageCreate.ts
@@ -1,10 +1,10 @@
-import { Events, Message } from "discord.js";
+import { Events, Message, TextChannel } from "discord.js";
 import Bot from "../Bot";
 
 module.exports = {
     name: Events.MessageCreate,
     execute(message: Message<boolean>) {
-        if (Bot.HIDDEN_CHANNELS.includes(message.channelId))
+        if ((message.channel as TextChannel).name.startsWith("hidden-announce"))
             Bot.getInstance().announce(message);
     }
 }
--- src/bot/events/messageUpdate.ts
+++ src/bot/events/messageUpdate.ts
@@ -1,10 +1,10 @@
-import { Events, Message, PartialMessage } from "discord.js";
+import { Events, Message, PartialMessage, TextChannel } from "discord.js";
 import Bot from "../Bot";
 
 module.exports = {
     name: Events.MessageUpdate,
     execute(oldMessage: Message<boolean> | PartialMessage, newMessage: Message<boolean> | PartialMessage) {
-        if (Bot.HIDDEN_CHANNELS.includes(newMessage.channelId))
+        if ((newMessage.channel as TextChannel).name.startsWith("hidden-announce"))
             Bot.getInstance().announce(newMessage, true);
     }
 }
