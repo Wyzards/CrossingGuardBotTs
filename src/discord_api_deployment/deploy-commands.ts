68672045672d6a5997f1fdd2d80cfb28fd3fd587
Theeef <realstarkr@gmail.com>
2025-07-20 03:37:55 -0400

    deploy commands

--- src/discord_api_deployment/deploy-commands.ts
+++ src/discord_api_deployment/deploy-commands.ts
@@ -4,45 +4,40 @@ import * as path from 'path';
 import { pathToFileURL } from 'url';
 
 const commands: any[] = [];
-// Grab all the command files from the commands directory you created earlier
 const foldersPath = path.join(process.cwd(), 'dist/bot/commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-    // Grab all the command files from the commands directory you created earlier
     const commandsPath = path.join(foldersPath, folder);
     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+
     for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
         const command = await import(pathToFileURL(filePath).toString());
         if ('data' in command && 'execute' in command) {
             commands.push(command.data.toJSON());
         } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+            console.warn(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
         }
     }
 }
 
-// Construct and prepare an instance of the REST module
-var configData = fs.readFile("config.json", 'utf8', (err, data) => {
-    const config = JSON.parse(data);
-    const rest = new REST().setToken(config["TOKEN"]);
+// Read config synchronously to avoid async/callback race conditions
+const config = JSON.parse(fs.readFileSync("config.json", 'utf8'));
 
-    // and deploy your commands!
-    (async () => {
-        try {
-            console.log(`Started refreshing ${commands.length} application (/) commands.`);
+const rest = new REST().setToken(config["TOKEN"]);
 
-            // The put method is used to fully refresh all commands in the guild with the current set
-            const data = await rest.put(
-                Routes.applicationGuildCommands(config["CLIENT_ID"], config["GUILD_ID"]),
-                { body: commands },
-            );
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-            console.log(`Successfully reloaded application (/) commands.`);
-        } catch (error) {
-            console.error(error);
-        }
-    })();
-});
+        const data = await rest.put(
+            Routes.applicationGuildCommands(config["CLIENT_ID"], config["GUILD_ID"]),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded application (/) commands.`);
+    } catch (error) {
+        console.error('Failed to deploy commands:', error);
+    }
+})();
